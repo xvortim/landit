33efb43af20fc8df398f9b44ead6ab6c9c661e08
mitro <mitrovski2001@gmail.com>
2023-11-02 18:53:33 +0100

    Final V2

--- Assets/Scripts/hudUpdate.cs
+++ Assets/Scripts/hudUpdate.cs
@@ -18,7 +18,7 @@ public class hudUpdater : MonoBehaviour
 		labelElement.text =  "Throttle: " + Airplane.throttle.ToString("F0") + "%\n" + "Airspeed: " + (Airplane.rb.velocity.magnitude*3).ToString("F0") 
 										  + "kn\n" + "Altitude: " + (Airplane.altitude*4).ToString("F0") +  "ft\n";
 		if(Input.GetKeyDown(KeyCode.H))
-			helpElement.text =  "Throttle: SPACE\\LCTRL \nPitch: S\\W \nRoll: A\\D \nYaw: Q\\E \nFlaps: F\\G \nCamera POV: num1 -> num4\nPress X to remove help" ;
+			helpElement.text =  "Throttle: SPACE\\LCTRL \nPitch: S\\W \nRoll: A\\D \nYaw: Q\\E \nFlaps: F\\G \nCamera POV: num1 -> num4\nR to restart, M to go back to menu\nPress X to remove help" ;
 		else if(Input.GetKeyDown(KeyCode.X))
 			helpElement.text =  "" ;
 
