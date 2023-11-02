d3d0e5abe30b3d4a6648158f6b31ddab48ed3748
mitro <mitrovski2001@gmail.com>
2023-11-02 17:37:01 +0100

    Added compass and polished up

--- Assets/Scripts/hudUpdate.cs
+++ Assets/Scripts/hudUpdate.cs
@@ -5,19 +5,23 @@ public class hudUpdater : MonoBehaviour
 {
     public Label labelElement;
 	public Label helpElement;
+	public Label compassElement;
 
     public void Start()
     {
-		labelElement = GetComponent<UIDocument>().rootVisualElement.Q<Label>("hud");
-		helpElement = GetComponent<UIDocument>().rootVisualElement.Q<Label>("help");
+		labelElement   = GetComponent<UIDocument>().rootVisualElement.Q<Label>("hud");
+		helpElement    = GetComponent<UIDocument>().rootVisualElement.Q<Label>("help");
+		compassElement = GetComponent<UIDocument>().rootVisualElement.Q<Label>("compass");
     }
 	
 	public void Update() {
 		labelElement.text =  "Throttle: " + Airplane.throttle.ToString("F0") + "%\n" + "Airspeed: " + (Airplane.rb.velocity.magnitude*3).ToString("F0") 
-										  + "kn\n" + "Altitude: " + (Airplane.altitude*4).ToString("F0") +  "ft\n" + Airplane.rb.transform.localRotation.x;
+										  + "kn\n" + "Altitude: " + (Airplane.altitude*4).ToString("F0") +  "ft\n";
 		if(Input.GetKeyDown(KeyCode.H))
 			helpElement.text =  "Throttle: SPACE\\LCTRL \nPitch: S\\W \nRoll: A\\D \nYaw: Q\\E \nFlaps: F\\G \nCamera POV: num1 -> num4\nPress X to remove help" ;
 		else if(Input.GetKeyDown(KeyCode.X))
-			helpElement.text =  "" ;		
+			helpElement.text =  "" ;
+
+		compassElement.text = (Airplane.rb.transform.eulerAngles.y).ToString("F0") + '°';
 	}
 }
